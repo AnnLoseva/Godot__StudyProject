366f5dd876259e44d93494a1c6c2da0d58482b02
Ann Loseva <95305121+AnnLoseva@users.noreply.github.com>
2023-10-25 22:00:44 +0300

    Double Jump

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+var double_jump = false
+var is_flying = false
+var flying_force = -200
+
+
 @onready var anim = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -14,10 +20,19 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		anim.play("Fall")
+		
+	else:
+		double_jump = false
+		is_flying = false
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim.play("Jump")
+		
+	elif Input.is_action_just_pressed("ui_accept") and !double_jump:
+		velocity.y = JUMP_VELOCITY
+		double_jump = true
 		anim.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
