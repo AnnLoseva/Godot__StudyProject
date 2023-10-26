6dc52319a85377ef352722fcce2efa5752651daf
Ann Loseva <95305121+AnnLoseva@users.noreply.github.com>
2023-10-26 20:32:40 +0300

    Lesson 6 Enemy

--- Mobs/skeleton.gd
+++ Mobs/skeleton.gd
@@ -6,20 +6,27 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var speed = 100
 var chase = false
-
-
+var alive = true
+@onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	var player = $"../../Player/Player"
-	var direction = (player.position - self.position).normalized()
-	if chase == true:
-		velocity.x = direction.x * speed
-	else:
-		velocity.x = 0
-	move_and_slide()
+	if alive == true:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+		var player = $"../../Player/Player"
+		var direction = (player.position - self.position).normalized()
+		if direction.x < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+		if chase == true:
+			velocity.x = direction.x * speed
+			anim.play("Run")
+		else:
+			velocity.x = 0
+			anim.play("Idle")
+		move_and_slide()
 
 
 func _on_detector_body_entered(body):
@@ -30,3 +37,24 @@ func _on_detector_body_entered(body):
 func _on_detector_body_exited(body):
 	if body.name == "Player":
 		chase = false
+
+func death():
+	alive = false
+	anim.play("Death")
+	await anim.animation_finished
+	queue_free()
+
+func _on_death_box_body_entered(body):
+	if body.name == "Player" and alive == true:
+		body.velocity.y = -300
+		$CollisionShape2D.queue_free()
+		death()
+
+
+func _on_hit_enemy_box_body_entered(body):
+	if body.name == "Player" and alive == true:
+		var direction = (body.position - self.position).normalized()
+		body.velocity.x = direction.x * 1000
+		body.hp -= 40
+		$CollisionShape2D.queue_free()
+		death()
--- Player/Player.gd
+++ Player/Player.gd
@@ -7,7 +7,8 @@ const JUMP_VELOCITY = -400.0
 var double_jump = false
 var is_flying = false
 var flying_force = -200
-
+var hp = 100
+var alive = true
 
 @onready var anim = $AnimatedSprite2D
 
@@ -17,40 +18,51 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		anim.play("Fall")
-		
-	else:
-		double_jump = false
-		is_flying = false
+	if alive == true:
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			anim.play("Fall")
+			
+		else:
+			double_jump = false
+			is_flying = false
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anim.play("Jump")
-		
-	elif Input.is_action_just_pressed("ui_accept") and !double_jump:
-		velocity.y = JUMP_VELOCITY
-		double_jump = true
-		anim.play("Jump")
+		# Handle Jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			anim.play("Jump")
+			
+		elif Input.is_action_just_pressed("ui_accept") and !double_jump:
+			velocity.y = JUMP_VELOCITY
+			double_jump = true
+			anim.play("Jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-		
-		if velocity.y == 0:
-			anim.play("Run") 
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		anim.play("Idle")
-		
-	if direction == 1:
-		$AnimatedSprite2D.flip_h = false
-		
-	elif direction == -1:
-			$AnimatedSprite2D.flip_h = true 
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+			
+			if velocity.y == 0:
+				anim.play("Run") 
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			anim.play("Idle")
+			
+		if direction == 1:
+			$AnimatedSprite2D.flip_h = false
+			
+		elif direction == -1:
+				$AnimatedSprite2D.flip_h = true 
 
-	move_and_slide()
+		move_and_slide()
+		
+		if hp <= 0:
+			death()
+		
+func death():
+	alive = false
+	anim.play("Death")
+	await anim.animation_finished
+	queue_free()
+	get_tree().change_scene_to_file("res://Menu/menu.tscn")
