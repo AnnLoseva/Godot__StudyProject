46dc3568e53d84186d08b63eaac1e1ae30855809
Ann Loseva <95305121+AnnLoseva@users.noreply.github.com>
2023-10-26 22:07:33 +0300

    Floating

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,10 +5,10 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 var double_jump = false
-var is_flying = false
-var flying_force = -200
+var flying_force = 1
 var hp = 100
 var alive = true
+var accept_pressed_time = 0
 
 @onready var anim = $AnimatedSprite2D
 
@@ -19,15 +19,19 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if alive == true:
+			
 		if not is_on_floor():
 			velocity.y += gravity * delta
 			anim.play("Fall")
 			
 		else:
 			double_jump = false
-			is_flying = false
 
 		# Handle Jump.
+		if Input.is_action_pressed("ui_accept"):
+			accept_pressed_time += delta
+			
+		
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			anim.play("Jump")
@@ -36,12 +40,22 @@ func _physics_process(delta):
 			velocity.y = JUMP_VELOCITY
 			double_jump = true
 			anim.play("Jump")
+			
+		if Input.is_action_just_released("ui_accept"):
+			accept_pressed_time = 0
+			flying_force = 1
+			
+		if accept_pressed_time >= 0.1:
+			velocity.y +=  -0.5 * gravity * delta
+			flying_force = 0.8
+			
+
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.get_axis("ui_left", "ui_right")
 		if direction:
-			velocity.x = direction * SPEED
+			velocity.x = direction * SPEED * flying_force
 			
 			if velocity.y == 0:
 				anim.play("Run") 
