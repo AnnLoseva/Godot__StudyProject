1c98c7a290937feff23a7402687c12b160c98f62
Ann Loseva <95305121+AnnLoseva@users.noreply.github.com>
2023-11-11 19:36:14 +0200

    Lesson 8 (Input Map, Fixing Bugs)

--- Player/Player.gd
+++ Player/Player.gd
@@ -29,20 +29,20 @@ func _physics_process(delta):
 			double_jump = false
 
 		# Handle Jump.
-		if Input.is_action_pressed("ui_accept"):
+		if Input.is_action_pressed("jump"):
 			accept_pressed_time += delta
 			
 		
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			anim.play("Jump")
 			
-		elif Input.is_action_just_pressed("ui_accept") and !double_jump:
+		elif Input.is_action_just_pressed("jump") and !double_jump:
 			velocity.y = JUMP_VELOCITY
 			double_jump = true
 			anim.play("Jump")
 			
-		if Input.is_action_just_released("ui_accept"):
+		if Input.is_action_just_released("jump"):
 			accept_pressed_time = 0
 			flying_force = 1
 			
@@ -54,7 +54,7 @@ func _physics_process(delta):
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction = Input.get_axis("ui_left", "ui_right")
+		var direction = Input.get_axis("left", "right")
 		if direction:
 			velocity.x = direction * SPEED * flying_force
 			
